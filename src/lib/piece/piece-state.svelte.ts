157d6c84e1b8b4ad5b4cb7077bc6d34f0d16dce4
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-01-02 19:41:08 -0500

    Give each piece a unique identity

--- src/lib/piece/piece-state.svelte.ts
+++ src/lib/piece/piece-state.svelte.ts
@@ -5,21 +5,23 @@ import { type Role, type Player, type Position } from "$lib/ruleset"
 export class Piece {
 	role: Role
 	player: Player
+	idx: number
 
 	pos: Position = $state({ row: 0, col: 0 })
 	offBoard: boolean = $state(true)
 
-	constructor(role: Role, player: Player, pos: Position) {
+	constructor(role: Role, player: Player, idx: number, pos: Position) {
 		this.role = role
 		this.player = player
+		this.idx = idx
 		this.pos = pos
 		this.offBoard = false
 	}
 }
 
 
-export function createPieceState(role: Role, player: Player, pos: Position) {
-	return setContext(Symbol(`${role}-${player}`), new Piece(role, player, pos))
+export function createPieceState(role: Role, player: Player, idx: number, pos: Position) {
+	return setContext(Symbol(`${role}-${player}-${idx}`), new Piece(role, player, idx, pos))
 }
 
 export function getPieceState(role: Role, player: Player) {
@@ -28,40 +30,40 @@ export function getPieceState(role: Role, player: Player) {
 
 export function createDefaultPieces() {
 	return [
-		createPieceState("rook", "black", { row: 0, col: 0 }),
-		createPieceState("knight", "black", { row: 0, col: 1 }),
-		createPieceState("bishop", "black", { row: 0, col: 2 }),
-		createPieceState("king", "black", { row: 0, col: 3 }),
-		createPieceState("queen", "black", { row: 0, col: 4 }),
-		createPieceState("bishop", "black", { row: 0, col: 5 }),
-		createPieceState("knight", "black", { row: 0, col: 6 }),
-		createPieceState("rook", "black", { row: 0, col: 7 }),
+		createPieceState("rook", "black", 1, { row: 0, col: 0 }),
+		createPieceState("rook", "black", 2, { row: 0, col: 7 }),
+		createPieceState("knight", "black", 1, { row: 0, col: 1 }),
+		createPieceState("knight", "black", 2, { row: 0, col: 6 }),
+		createPieceState("bishop", "black", 1, { row: 0, col: 2 }),
+		createPieceState("bishop", "black", 2, { row: 0, col: 5 }),
+		createPieceState("king", "black", 1, { row: 0, col: 3 }),
+		createPieceState("queen", "black", 1, { row: 0, col: 4 }),
 
-		createPieceState("pawn", "black", { row: 1, col: 0 }),
-		createPieceState("pawn", "black", { row: 1, col: 1 }),
-		createPieceState("pawn", "black", { row: 1, col: 2 }),
-		createPieceState("pawn", "black", { row: 1, col: 3 }),
-		createPieceState("pawn", "black", { row: 1, col: 4 }),
-		createPieceState("pawn", "black", { row: 1, col: 5 }),
-		createPieceState("pawn", "black", { row: 1, col: 6 }),
-		createPieceState("pawn", "black", { row: 1, col: 7 }),
+		createPieceState("pawn", "black", 1, { row: 1, col: 0 }),
+		createPieceState("pawn", "black", 2, { row: 1, col: 1 }),
+		createPieceState("pawn", "black", 3, { row: 1, col: 2 }),
+		createPieceState("pawn", "black", 4, { row: 1, col: 3 }),
+		createPieceState("pawn", "black", 5, { row: 1, col: 4 }),
+		createPieceState("pawn", "black", 6, { row: 1, col: 5 }),
+		createPieceState("pawn", "black", 7, { row: 1, col: 6 }),
+		createPieceState("pawn", "black", 8, { row: 1, col: 7 }),
 
-		createPieceState("pawn", "white", { row: 6, col: 0 }),
-		createPieceState("pawn", "white", { row: 6, col: 1 }),
-		createPieceState("pawn", "white", { row: 6, col: 2 }),
-		createPieceState("pawn", "white", { row: 6, col: 3 }),
-		createPieceState("pawn", "white", { row: 6, col: 4 }),
-		createPieceState("pawn", "white", { row: 6, col: 5 }),
-		createPieceState("pawn", "white", { row: 6, col: 6 }),
-		createPieceState("pawn", "white", { row: 6, col: 7 }),
+		createPieceState("pawn", "white", 1, { row: 6, col: 0 }),
+		createPieceState("pawn", "white", 2, { row: 6, col: 1 }),
+		createPieceState("pawn", "white", 3, { row: 6, col: 2 }),
+		createPieceState("pawn", "white", 4, { row: 6, col: 3 }),
+		createPieceState("pawn", "white", 5, { row: 6, col: 4 }),
+		createPieceState("pawn", "white", 6, { row: 6, col: 5 }),
+		createPieceState("pawn", "white", 7, { row: 6, col: 6 }),
+		createPieceState("pawn", "white", 8, { row: 6, col: 7 }),
 
-		createPieceState("rook", "white", { row: 7, col: 0 }),
-		createPieceState("knight", "white", { row: 7, col: 1 }),
-		createPieceState("bishop", "white", { row: 7, col: 2 }),
-		createPieceState("king", "white", { row: 7, col: 3 }),
-		createPieceState("queen", "white", { row: 7, col: 4 }),
-		createPieceState("bishop", "white", { row: 7, col: 5 }),
-		createPieceState("knight", "white", { row: 7, col: 6 }),
-		createPieceState("rook", "white", { row: 7, col: 7 }),
+		createPieceState("rook", "white", 1, { row: 7, col: 0 }),
+		createPieceState("rook", "white", 2, { row: 7, col: 7 }),
+		createPieceState("knight", "white", 1, { row: 7, col: 1 }),
+		createPieceState("knight", "white", 2, { row: 7, col: 6 }),
+		createPieceState("bishop", "white", 1, { row: 7, col: 2 }),
+		createPieceState("bishop", "white", 2, { row: 7, col: 5 }),
+		createPieceState("king", "white", 1, { row: 7, col: 3 }),
+		createPieceState("queen", "white", 1, { row: 7, col: 4 }),
 	]
 }
